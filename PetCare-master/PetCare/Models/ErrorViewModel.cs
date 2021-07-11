b896b2adc64c7ae483b9fcb270d6aab5706f81ae
nikolovkiril <nikolov.kiril123@gmail.com>
2021-07-11 09:17:21 +0300

    Initial commit

---
+++ PetCare-master/PetCare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace PetCare.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
